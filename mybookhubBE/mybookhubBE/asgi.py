ddca8d871e7ca3cca37311d8fc48e2725de55139
Michael <michael27mic@gmail.com>
2025-05-19 21:31:14 +0100

    wk1d1

---
+++ mybookhubBE/mybookhubBE/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for mybookhubBE project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mybookhubBE.settings')
+
+application = get_asgi_application()
